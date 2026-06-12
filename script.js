cf01848cc9442642ec7b67efabbfd3c12971ca64
gam2000m-coder <gam2000m@gmail.com>
2026-06-12 19:59:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,5 +23,5 @@ function openPayment(url) {
  */
 function openContact() {
     // Замените 'your_psychologist_username' на свой юзернейм (например, 'anna_psy')
-    tg.openTelegramLink('@galaxy_feelings_bot');
+    tg.openTelegramLink('@da41da91');
 }
